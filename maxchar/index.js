0a19603188c8708a753a1cc92862873c2bb1b89f
mishael.harry <mishael.harry@etranzact.com>
2020-01-15 11:55:31 +0100

    implemented maxchar & fizzbuzz

--- maxchar/index.js
+++ maxchar/index.js
@@ -5,6 +5,22 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    let characters = {};
+
+    for (let character of str) {
+        characters[character] = characters[character] + 1 || 1;
+    }
+
+    let maxCount = 0;
+    let maxChar = "";
+    for (let character in characters) {
+        if (characters[character] > maxCount) {
+            maxCount = characters[character];
+            maxChar = character;
+        }
+    }
+    return maxChar;
+}
 
 module.exports = maxChar;
